d8bc1d99119297c913d9a520b08f0327a25c4e52
jmedinac1987 <jmedinac1987@gmail.com>
2018-07-23 12:57:20 -0500

    Creación de componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { LoginComponent } from './componets/login/login.component';
+import { SignupComponent } from './componets/signup/signup.component';
+import { ProfileComponent } from './componets/profile/profile.component';
+import { RequestResetComponent } from './componets/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './componets/password/response-reset/response-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    RequestResetComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule
